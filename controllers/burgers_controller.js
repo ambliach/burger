ba2504ad752def0accbf22274ccaacc2bd5f72c5
ambli <ambliach@gmail.com>
2019-06-23 17:35:07 -0400

    herokuAttempt

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -6,7 +6,7 @@ var express = require("express")
 
 var router = express.Router();
 
-var burger = require("../models/burger.js")
+var burger = require("../models/burger")
 
 router.get("/", function(req, res){
     burger.all(function(data){
