fc878ada909632d4cdd036e3c68c259435e29efc
ambli <ambliach@gmail.com>
2019-06-23 16:39:51 -0400

    nearingMVP

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -24,8 +24,26 @@ router.post("/api/burgers", function(req, res){
         req.body.devoured,
         function(result){
             res.json({ id: result.insertId});
-
         });
 });
 
+router.put("/api/burgers/:id", function(req, res){
+var condition = "id = " + req.params.id;
 
+console.log("condition", condition);
+
+ 
+    burger.update({
+        devoured: req.body.devoured
+    }, condition, function(result){
+        if (result.changedRows == 0) {
+            return res.status(404).end();
+        }
+        else {
+            res.status(200).end();
+        }
+    
+    });
+});
+
+module.exports = router;
