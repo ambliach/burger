36005f8733117dad94b182b2a8529d4c5a0cc1c6
ambli <ambliach@gmail.com>
2019-06-23 12:10:43 -0400

    connectionToDB

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,2 @@
+//what needs to happan in the database
+//when it will happen in the databse
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Whatsaburger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Angry Whopper", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Beyond burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("mushroom burger", true);
